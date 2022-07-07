32f7e5eddb7e2f1411f3e51d75bf71be166ec434
Moti <motiml77@gmail.com>
2022-07-07 22:21:56 +0300

    update Register

--- app/src/main/java/com/example/registration/RegisterActivity.java
+++ app/src/main/java/com/example/registration/RegisterActivity.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        backBtn=findViewById(R.id.btnRegister);
 
         TextView btn=findViewById(R.id.alreadyHaveAccount);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -23,23 +26,55 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
             }
         });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backLogin();
+            }
+        });
     }
 
-    public void displayToast(View v){
+    void backLogin() {
         EditText editText1 = (EditText) findViewById(R.id.inputUsername);
         String username = editText1.getText().toString();
         EditText editText2 = (EditText) findViewById(R.id.inputEmail);
         String email = editText2.getText().toString();
         EditText editText3 = (EditText) findViewById(R.id.inputPassword);
-        String password = editText2.getText().toString();
+        String password = editText3.getText().toString();
         EditText editText4 = (EditText) findViewById(R.id.inputConformPassword);
-        String conformPassword = editText2.getText().toString();
+        String conformPassword = editText4.getText().toString();
 
+        if (username.isEmpty()||email.isEmpty()||password.isEmpty()||conformPassword.isEmpty())
+            Toast.makeText(RegisterActivity.this, "Please fill in all fields",Toast.LENGTH_SHORT).show();
+        else if ((password.compareTo(conformPassword))!=0)
+            Toast.makeText(RegisterActivity.this, "Please fix the password",Toast.LENGTH_SHORT).show();
+        else {
+            Toast.makeText(RegisterActivity.this, "Account Created!!", Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+        }
+
+    }
+
+  /*  public void displayToast(View v){
+        EditText editText1 = (EditText) findViewById(R.id.inputUsername);
+        String username = editText1.getText().toString();
+        EditText editText2 = (EditText) findViewById(R.id.inputEmail);
+        String email = editText2.getText().toString();
+        EditText editText3 = (EditText) findViewById(R.id.inputPassword);
+        String password = editText3.getText().toString();
+        EditText editText4 = (EditText) findViewById(R.id.inputConformPassword);
+        String conformPassword = editText4.getText().toString();
 
         if (username.isEmpty()||email.isEmpty()||password.isEmpty()||conformPassword.isEmpty())
         Toast.makeText(RegisterActivity.this, "Please fill in all fields",Toast.LENGTH_SHORT).show();
-           else
+        else if ((password.compareTo(conformPassword))!=0)
+            Toast.makeText(RegisterActivity.this, "Please fix the password",Toast.LENGTH_SHORT).show();
+        else
          Toast.makeText(RegisterActivity.this, "Account Created!!",Toast.LENGTH_SHORT).show();
 
     }
+
+   */
 }
